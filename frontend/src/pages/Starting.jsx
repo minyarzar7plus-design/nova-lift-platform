b83b31bf5b000c44aeb186842a2fa1eaee362efe
minyarzar7plus-design <297132991+minyarzar7plus-design@users.noreply.github.com>
2026-08-06 09:50:29 +0700

    Refactor Starting component to include subcomponents

--- frontend/src/pages/Starting.jsx
+++ frontend/src/pages/Starting.jsx
@@ -1,1 +1,13 @@
-export { default } from './Starting';
+import ProductGrid from '../components/starting/ProductGrid';
+import Gauge from '../components/starting/Gauge';
+import NoticeBox from '../components/starting/NoticeBox';
+
+export default function Starting() {
+  return (
+    <div>
+      <Gauge />
+      <NoticeBox />
+      <ProductGrid />
+    </div>
+  );
+}
